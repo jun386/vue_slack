8127fb61e3c8754a02e7f10acde46696b7f956a4
jun386 <aka6116.aka@gmail.com>
2021-12-16 08:22:47 +0900

    messageフォーム作成

--- app/javascript/components/channels/show.vue
+++ app/javascript/components/channels/show.vue
@@ -5,7 +5,14 @@
     </div>
     <div class="main-content">
       <div class="container">
-        <messages />
+        <div class="chats-layout">
+          <messages />
+        </div>
+        <div class="input-layout">
+          <div class="input-container">
+            <textarea></textarea>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +40,29 @@ export default {
   width: 100%;
   background: #F1F1F1;
   height: 100vh;
+}
+
+.container {
+  height: 100%;
+}
+
+.chats-layout {
+  overflow: scroll;
+  height: 90%;
+}
+
+.input-layout {
+  height: 10%;
+}
+
+.input-container {
+  padding: 10px;
+  height: 100%;
+}
+
+textarea {
+  width: 100%;
+  height: 100%;
 }
 
 </style>
